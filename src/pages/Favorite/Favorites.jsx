2a7d4602e010a15526ee57fd881b29e29e38f289
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-09-15 10:37:06 +0300

    Create Favorites.jsx

---
+++ src/pages/Favorite/Favorites.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Favorites = () => {
+  return (
+    <div>
+      <h1>Улюблені оголошення</h1>
+      {/* Тут ви можете відображати список улюблених оголошень */}
+    </div>
+  );
+}
+
+export default Favorites;
