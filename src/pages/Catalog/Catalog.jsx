78084766e9144dbd5d907dc7a65940df704d641c
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-09-15 10:37:03 +0300

    Create Catalog.jsx

---
+++ src/pages/Catalog/Catalog.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Catalog = () => {
+  return (
+    <div>
+      <h1>Каталог автомобілів</h1>
+      {/* Тут ви можете розмістити список автомобілів та фільтри */}
+    </div>
+  );
+}
+
+export default Catalog;
