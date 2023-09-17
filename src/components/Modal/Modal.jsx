c6ceff9ba5251cdf364f87d437e7c2f6c37f2aa3
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-09-17 15:53:31 +0300

    Update Modal.jsx

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+import { Overlay, ModalContainer } from './Modal.styled';
+
+const Modal = ({ data, onClose }) => {
+  useEffect(() => {
+    const hendleKeyDown = event => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', hendleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', hendleKeyDown);
+    };
+  }, [onClose]);
+
+  const hendleBeckdropClick = event => {
+    if (event.currentTarget === event.target) {
+      onClose();
+    }
+  };
+
+  return (
+    <>
+      <Overlay onClick={hendleBeckdropClick}>
+        <ModalContainer>
+          <img src={data.img} alt="" />
+          <h1>{data.model}</h1>
+        </ModalContainer>
+      </Overlay>
+    </>
+  );
+};
+
+export default Modal;
