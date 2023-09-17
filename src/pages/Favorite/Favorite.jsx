6f26a7f2559d52a50dc2a9ba2d7af0e21ecb869c
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-09-17 15:53:48 +0300

    Create Favorite.jsx

---
+++ src/pages/Favorite/Favorite.jsx
@@ -0,0 +1,35 @@
+import {
+  Img,
+  Container,
+  ContainerCar,
+} from '../Catalog/Catalog.styled';
+
+const Favorite = ({ favorite }) => {
+  return (
+    <>
+      <Container>
+        {favorite.map(car => (
+          <ContainerCar key={car.id}>
+            <Img src={car.img} alt="car" />
+            <ul>
+              <li>{car.make}</li>
+              <li>{car.model}</li>
+              <li>{car.year}</li>
+              <li>{car.rentalPrice}</li>
+            </ul>
+            <ul>
+              <li>{car.address}</li>
+              <li>{car.rentalCompany}</li>
+              <li>{car.type}</li>
+              <li>{car.model}</li>
+              <li>{car.id}</li>
+              <li>{car.accessories[1]}</li>
+            </ul>
+          </ContainerCar>
+        ))}
+      </Container>
+    </>
+  );
+};
+
+export default Favorite;
