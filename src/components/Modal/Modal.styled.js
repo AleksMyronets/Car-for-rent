75e216d6956cfca6c7e9d2eeb2085760ed8e8ad5
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-09-19 14:11:54 +0300

    Update Modal.styled.js

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -1,23 +1,52 @@
 import styled from '@emotion/styled';
 
 const Overlay = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: rgba(0, 0, 0, 0.8);
-    z-index: 1200;
+  background: rgba(0, 0, 0, 0.7);
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
 `;
 
 const ModalContainer = styled.div`
-    max-width: calc(100vw - 48px);
-    max-height: calc(100vh - 24px);
-`
+  background: white;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  padding: 20px;
+  max-width: 600px;
+  width: 90%;
+  text-align: center;
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+`;
+
+const CarInfo = styled.div`
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+
+    li {
+      width: 50%; 
+      margin-bottom: 10px;
+    }
+  }
+`;
+
 export {
     Overlay,
-    ModalContainer
+    ModalContainer,
+    CarInfo
 };
