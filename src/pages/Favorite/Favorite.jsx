c22a55c031d866e8001c3635f6cd5a6ef8b07378
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-09-19 12:46:32 +0300

    Update Favorite.jsx

--- src/pages/Favorite/Favorite.jsx
+++ src/pages/Favorite/Favorite.jsx
@@ -2,32 +2,35 @@ import {
   Img,
   Container,
   ContainerCar,
+  ContainerMain,
 } from '../Catalog/Catalog.styled';
 
 const Favorite = ({ favorite }) => {
   return (
     <>
-      <Container>
-        {favorite.map(car => (
-          <ContainerCar key={car.id}>
-            <Img src={car.img} alt="car" />
-            <ul>
-              <li>{car.make}</li>
-              <li>{car.model}</li>
-              <li>{car.year}</li>
-              <li>{car.rentalPrice}</li>
-            </ul>
-            <ul>
-              <li>{car.address}</li>
-              <li>{car.rentalCompany}</li>
-              <li>{car.type}</li>
-              <li>{car.model}</li>
-              <li>{car.id}</li>
-              <li>{car.accessories[1]}</li>
-            </ul>
-          </ContainerCar>
-        ))}
-      </Container>
+      <ContainerMain>
+        <Container>
+          {favorite.map(car => (
+            <ContainerCar key={car.id}>
+              <Img src={car.img} alt="car" />
+              <ul>
+                <li>{car.make}</li>
+                <li>{car.model}</li>
+                <li>{car.year}</li>
+                <li>{car.rentalPrice}</li>
+              </ul>
+              <ul>
+                <li>{car.address}</li>
+                <li>{car.rentalCompany}</li>
+                <li>{car.type}</li>
+                <li>{car.model}</li>
+                <li>{car.id}</li>
+                <li>{car.accessories[1]}</li>
+              </ul>
+            </ContainerCar>
+          ))}
+        </Container>
+      </ContainerMain>
     </>
   );
 };
