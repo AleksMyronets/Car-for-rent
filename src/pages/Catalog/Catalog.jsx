558380382e45be8429e867a65dc3c939b9fb1d49
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-09-22 18:43:16 +0300

    Update Catalog.jsx

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,186 +1,5 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-// import {
-//   Img,
-//   Container,
-//   ContainerCar,
-//   Icon,
-//   BtnIcon,
-//   LearnMoreButton,
-//   BtnLodeMore,
-//   Form,
-//   ContainerMain,
-// } from './Catalog.styled';
-
-
-// const FavoriteButton = ({ car, addFavorit }) => {
-//   const [isIconBlue, setIsIconBlue] = useState(false);
-
-//   const toggleIconColor = () => {
-//     setIsIconBlue(!isIconBlue);
-//     addFavorit(car);
-//   };
-
-//   return (
-//     <BtnIcon onClick={toggleIconColor}>
-//       <Icon id={car.id} style={{ color: isIconBlue ? 'blue' : 'initial' }} />
-//     </BtnIcon>
-//   );
-// };
-
-// const CatalogPage = ({
-//   cars,
-//   onClick,
-//   onClickLoadeMore,
-//   addFavorit,
-//   onLoadeMore,
-//   setIconColor,
-// }) => {
-//   const [brandFilter, setBrandFilter] = useState('');
-//   const [priceFilter, setPriceFilter] = useState('');
-//   const [fromMileage, setFromMileage] = useState('');
-//   const [toMileage, setToMileage] = useState('');
-
-//   const [filteredCars, setFilteredCars] = useState([]);
-//   const [loadedCarsCount, setLoadedCarsCount] = useState(6);
-//   const [carsPerPage] = useState(6);
-
-//   const handleSearch = async () => {
-//     try {
-//       const response = await axios.get(
-//         'https://6492c7dd428c3d2035d0ab50.mockapi.io/adverts',
-//         {
-//           params: {
-//             make: brandFilter,
-//             rentalPrice: priceFilter,
-//             fromMileage: fromMileage,
-//             toMileage: toMileage,
-//           },
-//         }
-//       );
-
-//       if (response.data && typeof response.data === 'object') {
-        
-//         setFilteredCars(response.data);
-//       } else {
-        
-//         console.error('Response is not valid JSON:', response.data);
-//       }
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-
-//   const handleLoadMore = () => {
-//     setLoadedCarsCount(loadedCarsCount + carsPerPage);
-//   };
-
-//   useEffect(() => {
-//     handleSearch();
-//   });
-    
-    
-
-//   return (
-//     <>
-//       <ContainerMain>
-//         <Form>
-//           <label>Car brand</label>
-//           <select
-//             value={brandFilter}
-//             onChange={e => setBrandFilter(e.target.value)}
-//           >
-//             <option value="">All</option>
-//             <option value="Buick">Buick</option>
-//             <option value="Volvo">Volvo</option>
-//             <option value="HUMMER">HUMMER</option>
-//             <option value="Subaru">Subaru</option>
-//             <option value="Mitsubishi">Mitsubishi</option>
-//             <option value="Nissan">Nissan</option>
-//             <option value="Lincoln">Lincoln</option>
-//             <option value="GMC">GMC</option>
-//             <option value="Hyundai">Hyundai</option>
-//             <option value="MINI">MINI</option>
-//             <option value="Bentley">Bentley</option>
-//             <option value="Mercedes-Benz">Mercedes-Benz</option>
-//             <option value="Aston Martin">Aston Martin</option>
-//             <option value="Pontiac">Pontiac</option>
-//             <option value="Lamborghini">Lamborghini</option>
-//             <option value="Audi">Audi</option>
-//             <option value="BMW">BMW</option>
-//             <option value="Chevrolet">Chevrolet</option>
-//             <option value="Chrysler">Chrysler</option>
-//             <option value="Kia">Kia</option>
-//             <option value="Land">Land</option>
-//           </select>
-//           <label>Price/1 hour</label>
-//           <select
-//             value={priceFilter}
-//             onChange={e => setPriceFilter(e.target.value)}
-//           >
-//             <option value="">Any</option>
-//             {Array.from({ length: 16 }, (_, index) => (
-//               <option key={index} value={(index + 1) * 10}>
-//                 ${(index + 1) * 10}
-//               </option>
-//             ))}
-//           </select>
-//           <label>Car mileage / km (From)</label>
-//           <input
-//             type="number"
-//             value={fromMileage}
-//             onChange={e => setFromMileage(e.target.value)}
-//           />
-//           <label>To</label>
-//           <input
-//             type="number"
-//             value={toMileage}
-//             onChange={e => setToMileage(e.target.value)}
-//           />
-//           <button type="button" onClick={handleSearch}>
-//             Search
-//           </button>
-//         </Form>
-
-//         <Container>
-//           {filteredCars.slice(0, loadedCarsCount).map(car => (
-//             <ContainerCar key={car.id}>
-//               <Img src={car.img} alt="car" />
-//               <BtnIcon onClick={() => addFavorit(car)}>
-//                 <Icon id={car.id} />
-//               </BtnIcon>
-//               <FavoriteButton car={car} addFavorit={addFavorit} />
-//               <ul>
-//                 <li>{car.make}</li>
-//                 <li>{car.model}</li>
-//                 <li>{car.year}</li>
-//                 <li>{car.rentalPrice}</li>
-//               </ul>
-//               <ul>
-//                 <li>{car.address}</li>
-//                 <li>{car.rentalCompany}</li>
-//                 <li>{car.type}</li>
-//                 <li>{car.id}</li>
-//                 <li>{car.accessories[1]}</li>
-//               </ul>
-//               <LearnMoreButton onClick={() => onClick(car)}>
-//                 Learn more
-//               </LearnMoreButton>
-//             </ContainerCar>
-//           ))}
-//         </Container>
-//         {loadedCarsCount < filteredCars.length && (
-//           <BtnLodeMore onClick={handleLoadMore}>Load more</BtnLodeMore>
-//         )}
-//       </ContainerMain>
-//     </>
-//   );
-// };
-
-// export default CatalogPage;
-
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { fetchCars } from '../../components/api';
 import {
   Img,
   Container,
@@ -188,7 +7,7 @@ import {
   Icon,
   BtnIcon,
   LearnMoreButton,
-  BtnLodeMore,
+  BtnLoadMore,
   Form,
   ContainerMain,
 } from './Catalog.styled';
@@ -211,9 +30,9 @@ const FavoriteButton = ({ car, addFavorit }) => {
 const CatalogPage = ({
   cars,
   onClick,
-  onClickLoadeMore,
-  addFavorit,
-  onLoadeMore,
+  onClickLoadMore,
+  addFavorite,
+  onLoadMore,
   setIconColor,
 }) => {
   const [brandFilter, setBrandFilter] = useState('');
@@ -222,8 +41,8 @@ const CatalogPage = ({
   const [toMileage, setToMileage] = useState('');
 
   const [filteredCars, setFilteredCars] = useState([]);
-  const [loadedCarsCount, setLoadedCarsCount] = useState(6);
-  const [carsPerPage] = useState(6);
+  const [loadedCarsCount, setLoadedCarsCount] = useState(8);
+  const [carsPerPage] = useState(8);
 
   const [isFetching, setIsFetching] = useState(false);
 
@@ -232,30 +51,23 @@ const CatalogPage = ({
       setIsFetching(true);
 
       try {
-        const response = await axios.get(
-          'https://6492c7dd428c3d2035d0ab50.mockapi.io/adverts',
-          {
-            params: {
-              make: brandFilter,
-              rentalPrice: priceFilter,
-              fromMileage: fromMileage,
-              toMileage: toMileage,
-            },
-          }
-        );
+        const params = {
+          make: brandFilter,
+          rentalPrice: priceFilter,
+          fromMileage: fromMileage,
+          toMileage: toMileage,
+        };
 
-        if (response.data && typeof response.data === 'object') {
-          setFilteredCars(response.data);
-        } else {
-          console.error('Response is not valid JSON:', response.data);
-        }
+        const carsData = await fetchCars(params); 
+
+        setFilteredCars(carsData); 
       } catch (error) {
         console.error(error);
       }
 
       setTimeout(() => {
         setIsFetching(false);
-      }, 1000); // Затримка в мілісекундах (в даному випадку 1000 мс = 1 секунда)
+      }, 2000);
     }
   };
 
@@ -265,7 +77,7 @@ const CatalogPage = ({
 
   useEffect(() => {
     handleSearch();
-  }, [brandFilter, priceFilter, fromMileage, toMileage]);
+  });
 
   return (
     <>
@@ -278,7 +90,8 @@ const CatalogPage = ({
           >
             <option value="">All</option>
             <option value="Buick">Buick</option>
-            {/* Додайте інші опції для брендів */}
+            <option value="Volvo">Volvo</option>
+            {/* Додайте інші опції брендів */}
           </select>
 
           <label>Price/1 hour</label>
@@ -317,10 +130,10 @@ const CatalogPage = ({
           {filteredCars.slice(0, loadedCarsCount).map(car => (
             <ContainerCar key={car.id}>
               <Img src={car.img} alt="car" />
-              <BtnIcon onClick={() => addFavorit(car)}>
+              <BtnIcon onClick={() => addFavorite(car)}>
                 <Icon id={car.id} />
               </BtnIcon>
-              <FavoriteButton car={car} addFavorit={addFavorit} />
+              <FavoriteButton car={car} addFavorit={addFavorite} />
               <ul>
                 <li>{car.make}</li>
                 <li>{car.model}</li>
@@ -341,7 +154,7 @@ const CatalogPage = ({
           ))}
         </Container>
         {loadedCarsCount < filteredCars.length && (
-          <BtnLodeMore onClick={handleLoadMore}>Load more</BtnLodeMore>
+          <BtnLoadMore onClick={handleLoadMore}>Load more</BtnLoadMore>
         )}
       </ContainerMain>
     </>
